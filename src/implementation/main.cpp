eabf95a1896d16f8b5d62331004764c45cf31b13
jfmurillo <joseph.murillo1621@gmail.com>
2024-07-05 12:27:11 -0600

    feat: created main

---
+++ src/implementation/main.cpp
@@ -0,0 +1,10 @@
+#include "GameMenu.h"
+#include <iostream>
+
+int main() {
+    GameMenu game;
+    game.showGameMenu();
+
+    std::cout << "=== Exiting Program ===" << std::endl;
+    return 0;
+}
